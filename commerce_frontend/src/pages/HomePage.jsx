c326eacd916191a3cdce115c82bd00df50fa1bb3
Ayman Samir <aymansamir45@gmail.com>
2022-06-26 18:05:02 +0200

    direction

--- commerce_frontend/src/pages/HomePage.jsx
+++ commerce_frontend/src/pages/HomePage.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Signup from "../components/register-user/Signup";
+import "./home.scss"
+import pic1 from "../public/mainfactor.jpg"
 
 const HomePage = () => {
   return (
-    <div>
-      <Signup />
+    <div className="home-page">
+      <div className="content-page">
+        <section className="section-1">
+<div className="section-content">
+<div className="img">
+    <img src={pic1} alt="" className="pic1"/>
+    </div>
+  <div className="art">
+  <div className="section-label">
+    <h1 >لماذا نحن</h1>
+    </div>
+  <p className="article">
+    Lorem ipsum dolor, <br/>
+    sit amet consectetur adipisicing elit.<br/>
+    Sint sapiente fuga quos vel laborum pariatur fugiat tempore magnam ea!<br/>
+    Voluptates mollitia tenetur fugiat ea corrupti!<br/>
+    Autem corporis voluptates voluptatem placeat.
+  </p>
+  </div>
+</div>
+        </section>
+      </div>
+      
     </div>
   );
 };
