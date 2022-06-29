69cc4509fb50e740552f510812948fe8752b323f
Ayman Samir <87248114+DeathL1sT@users.noreply.github.com>
2022-06-30 00:40:33 +0200

    icon

--- commerce_frontend/src/components/footer/Footer.jsx
+++ commerce_frontend/src/components/footer/Footer.jsx
@@ -1,31 +1,58 @@
-import React from 'react'
-import "./footer.scss"
+import React from "react";
+import {
+  WhatsAppOutlined,
+  MobileTwoTone,
+  HomeTwoTone,
+} from "@ant-design/icons";
+import "./footer.scss";
 function Footer() {
-  const contacts ={
-    id : "owner",
-    mobile:"01090963154",
-    whatsapp:"01090963154",
-    address: "ش51 مسجد الهدايا الطوابق فيصل - الجيزة"
-  }
+  const contacts = {
+    id: "owner",
+    mobile: "01090963154",
+    whatsapp: "01090963154",
+    address: "ش51 مسجد الهدايا الطوابق فيصل - الجيزة",
+  };
   return (
-    <div className= "footer">
-<div className='fotter-content'>
-<div>
-        <h6>Copy right &copy;2022</h6>
-      </div>
-        <div className='contact'>
-        <ul>
-          <li>{contacts.address}</li>
-          <li>{contacts.mobile}</li>
-          <li>{contacts.whatsapp}</li>
-        </ul>
-           
-      
+    <div className="footer">
+      <div className="fotter-content">
+        <div>
+          <h6>Copy right &copy;2022</h6>
         </div>
-</div>
-        
+        <div className="contact">
+          <ul>
+            <li>
+              <HomeTwoTone
+                style={{
+                  fontSize: "larger",
+                  margin: "8px",
+                }}
+              />
+              {contacts.address}
+            </li>
+            <li>
+              <MobileTwoTone
+                style={{
+                  fontSize: "larger",
+                  margin: "8px",
+                }}
+              />
+              {contacts.mobile}
+            </li>
+            <li>
+              <WhatsAppOutlined
+                style={{
+                  color: "greenyellow",
+                  fontSize: "larger",
+                  margin: "8px",
+                }}
+              />
+              {contacts.whatsapp}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
